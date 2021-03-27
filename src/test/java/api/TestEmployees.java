92a3c2af430fc2ee0442f04d8dc53214d95d7307
Amanajás <4821968+amanajas@users.noreply.github.com>
2021-03-27 17:26:29 +0100

    Update TestEmployees.java

--- src/test/java/api/TestEmployees.java
+++ src/test/java/api/TestEmployees.java
@@ -19,17 +19,15 @@ public class TestEmployees {
     public static final String API_URL = "http://127.0.0.1:5002/employees";
 
     @Test
-    @Order(1)
     public void testStatus() {
         given().when().get(API_URL).then().assertThat().statusCode(200);
     }
     @Test
-    @Order(2)
     public void testResponseTime() { given().when().get(API_URL).then().time(lessThan(500L)); }
 
     @Test
-    @Order(3)
-    public void testGetListSchema() {
+    @Order(1)
+    public void testListEmployees() {
         given().when().get(API_URL)
                 .then()
                     .log().ifValidationFails()
@@ -40,23 +38,11 @@ public class TestEmployees {
     }
 
     @Test
-    @Order(4)
+    @Order(2)
     public void testCreateSuccessResponse() {
         JSONObject requestParams = new JSONObject();
         requestParams.put("first_name", "Test");
         requestParams.put("last_name", "API");
-        requestParams.put("address", "Worldwide 123");
-        requestParams.put("birth_date", "2000-03-13");
-        requestParams.put("city", "Muenchen");
-        requestParams.put("country", "Germnay");
-        requestParams.put("email", "testapi@mailinator.com");
-        requestParams.put("fax", "1123213123");
-        requestParams.put("hire_date", "2017-09-25");
-        requestParams.put("phone", "12345678987");
-        requestParams.put("postal_code", "80683");
-        requestParams.put("reports_to", 0);
-        requestParams.put("state", "BA");
-        requestParams.put("title", "Software Tester");
         Response response = given()
                 .contentType(ContentType.TEXT)
                 .baseUri(API_URL)
@@ -64,27 +50,24 @@ public class TestEmployees {
                 .post("/create")
                 .then()
                     .statusCode(200)
-                    .contentType(ContentType.JSON)
-                .body(matchesJsonSchemaInClasspath("employeesCreateSuccess.json")).extract().response();
+                    .contentType(ContentType.JSON).extract().response();
         Assertions.assertTrue(response.jsonPath().getString("success").contains("Employee created"));
     }
 
     @Test
-    @Order(5)
+    @Order(3)
     public void testDeleteSuccessResponse() {
         Response response = given().baseUri(API_URL).post("/delete/last")
                 .then()
                     .log().ifValidationFails()
                     .assertThat()
                     .statusCode(200)
-                .contentType(ContentType.JSON)
-                .body(matchesJsonSchemaInClasspath("employeesDeleteSuccess.json")).extract().response();
+                .contentType(ContentType.JSON).extract().response();
         Assertions.assertTrue(response.jsonPath().getString("success").contains("Number of rows deleted 1"));
     }
 
     @Test
     public void testGetDataSchema() {
-        // INFO: It suppose to have an employee with ID 1
         given().when().baseUri(API_URL).get("/1")
                 .then()
                 .log().ifValidationFails()
@@ -97,7 +80,6 @@ public class TestEmployees {
 
     @Test
     public void testDeleteSkippedResponse() {
-        // INFO: It suppose to not have an employee with ID -1
         Response response = given().baseUri(API_URL).urlEncodingEnabled(true)
                 .queryParam("employee_id", -1)
                 .header("Accept", ContentType.JSON.getAcceptHeader())
@@ -106,8 +88,7 @@ public class TestEmployees {
                     .log().ifValidationFails()
                     .assertThat()
                     .statusCode(200)
-                .contentType(ContentType.JSON)
-                .body(matchesJsonSchemaInClasspath("employeesDeleteSkipped.json")).extract().response();
+                .contentType(ContentType.JSON).extract().response();
         Assertions.assertTrue(response.jsonPath().getString("skipped").contains("No employee was deleted"));
     }
 
